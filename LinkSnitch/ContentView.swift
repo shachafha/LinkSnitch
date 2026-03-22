1438749e364b4431a4ea62a6129afbf7a9ba91bc
shacahf rispler <shachaf.haviv@gmail.com>
2026-03-22 11:47:03 -0400

    Initial Commit

---
+++ LinkSnitch/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LinkSnitch
+//
+//  Created by shachaf haviv on 22/03/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LinkSnitch/LinkSnitchApp.swift
@@ -0,0 +1,17 @@
+//
+//  LinkSnitchApp.swift
+//  LinkSnitch
+//
+//  Created by shachaf haviv on 22/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct LinkSnitchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
